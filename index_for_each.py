0e394385c9d83bb8aae58c64c475e56c936e5d72
ospitia <o.espitiamendoza@campus.unimib.it>
2022-04-08 00:28:01 +0200

    adding data preparation for reranking

--- index_for_each.py
+++ index_for_each.py
@@ -1,12 +1,11 @@
 import os
-
+import yaml
 import pandas as pd
 import pyterrier as pt
+from src.data.utils.build_samples_4_reranking import build_samples_4_reranking
 
 if not pt.started():
-    pt.init(mem=8000, version='snapshot',
-            boot_packages=["com.github.terrierteam:terrier-prf:-SNAPSHOT"]
-            )
+    pt.init()
 
 
 def combine_result(data_path, model_name, data_to_index, dtype='train'):
@@ -15,23 +14,23 @@ def combine_result(data_path, model_name, data_to_index, dtype='train'):
         if os.path.isfile(f'''{data_path}{lang}_{data_to_index}_{model_name}_{dtype}.csv'''):
             sub_df = pd.read_csv(f'''{data_path}{lang}_{data_to_index}_{model_name}_{dtype}.csv''', index_col=0)
             if dtype == 'train':
-                sub_df=sub_df[['qid','docno','score']]
-                sub_df.qid=sub_df.qid.astype(str)
+                sub_df = sub_df[['qid', 'docno', 'score']]
+                sub_df.qid = sub_df.qid.astype(str)
                 sub_df.docno = sub_df.docno.astype(str)
-                #sub_df.columns=['query_id','product_id','score']
+                # sub_df.columns=['query_id','product_id','score']
             final.append(sub_df)
 
-
     df = pd.concat(final)
-    if dtype=='test':
+    if dtype == 'test':
         df.query_id = df.query_id.astype(str)
-        df.product_id=df.product_id.astype(str)
+        df.product_id = df.product_id.astype(str)
     df.to_csv(f'''{data_to_index}_{model_name}_{dtype}.csv''', index=None)
     return df
 
+
 def get_merged_qrels(data_path):
-    final=[]
-    for lang in ['en','es','jp']:
+    final = []
+    for lang in ['en', 'es', 'jp']:
         if os.path.isfile(f'''{data_path}{lang}_qrels.csv'''):
             qrels = pd.read_csv(f'''{data_path}{lang}_qrels.csv''', sep=',')
             qrels['label'] = qrels['label'].astype(int)
@@ -39,6 +38,7 @@ def get_merged_qrels(data_path):
             final.append(qrels)
     df = pd.concat(final)
     return df
+
 
 def get_qrels(data_path, lang):
     qrels = pd.read_csv(f'''{data_path}{lang}_qrels.csv''', sep=',')
@@ -49,7 +49,6 @@ def get_qrels(data_path, lang):
 
 def index_each_lang(qe=False, eval=True, lang='en', index_path='', gs=False, data_to_index='title_text',
                     change_settings=False):
-
     data_type = f'''{lang}_{data_to_index}'''
     df_docs = pd.read_csv(f'''{data_path}{lang}_prod_sub.csv''')
     if not os.path.exists(f'''{index_path}{data_type}/data.properties'''):
@@ -60,12 +59,12 @@ def index_each_lang(qe=False, eval=True, lang='en', index_path='', gs=False, dat
 
     print('-----------------Indexed----------------\n')
 
-    if lang=='en':
-        stemmer='Stopwords,PorterStemmer'
-    elif lang=='es':
-        stemmer='Stopwords,SpanishSnowballStemmer'
+    if lang == 'en':
+        stemmer = 'Stopwords,PorterStemmer'
+    elif lang == 'es':
+        stemmer = 'Stopwords,SpanishSnowballStemmer'
     else:
-        stemmer=''
+        stemmer = ''
     BM25 = pt.BatchRetrieve(indexref3, num_results=30, wmodel="BM25",
                             controls={"c": 0.8, "bm25.k_1": 0.6, "bm25.k_3": 0.5}, properties={
             'tokeniser': 'UTFTokeniser',
@@ -100,6 +99,9 @@ def index_each_lang(qe=False, eval=True, lang='en', index_path='', gs=False, dat
         qrels = get_qrels(data_path, lang)
         topics = pt.io.read_topics(f'''{data_path}{lang}_topics.csv''', format='singleline', tokenise=True)
         res = BM25.transform(topics)
+
+        build_samples_4_reranking(res, qrels)
+
         res.to_csv(f'''{data_path}{lang}_{data_to_index}_{model_name}_train.csv''')
         print(pt.Utils.evaluate(res, qrels, metrics=['ndcg_cut_10', 'ndcg_cut_20', 'ndcg']))
 
@@ -112,12 +114,17 @@ def index_each_lang(qe=False, eval=True, lang='en', index_path='', gs=False, dat
     submission_sample.to_csv(f'''{data_path}{lang}_{data_to_index}_{model_name}_test.csv''')
 
 
-data_path = '/Users/ricky/PycharmProjects/KDD_amazon/subsets/'
-index_path = '/Users/ricky/Documents/Rishabh/Dataset/KDD_amazon/index/'
-data_to_index = 'title_text'
-for lang in ['es','jp']:
-    index_each_lang(qe=False, eval=True, lang=lang, index_path=index_path, gs=False)
-combined_df=combine_result(data_path, 'BM25', data_to_index, dtype='test')
-qrels=get_merged_qrels(data_path)
-pt.Utils.evaluate(combined_df, qrels, metrics=['ndcg_cut_10', 'ndcg_cut_20'])
-#{'ndcg_cut_10': 0.23747557699680494, 'ndcg_cut_20': 0.22943625462979889}
+if __name__ == '__main__':
+    with open('./config.yml') as f:
+        config = yaml.load(f, Loader=yaml.FullLoader)['dataset']
+    data_path = './subsets/'
+    index_path = config["path"]
+    data_to_index = 'title_text'
+    for lang in ['es', 'jp']:
+        index_each_lang(qe=False, eval=True, lang=lang, index_path=index_path, gs=False)
+    combined_df = combine_result(data_path, 'BM25', data_to_index, dtype='test')
+    qrels = get_merged_qrels(data_path)
+    # print(pt.Utils.evaluate(combined_df, qrels, metrics=['ndcg_cut_10', 'ndcg_cut_20']))
+    # {'ndcg_cut_10': 0.23747557699680494, 'ndcg_cut_20': 0.22943625462979889}
+    # "{'ndcg_cut_10': 0.25315503702420733, 'ndcg_cut_20': 0.23043245567606602, 'ndcg': 0.21924521209667988}
+
