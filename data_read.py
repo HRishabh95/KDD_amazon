486ce9b9edc329e3de848354e32ba70ed3769884
ospitia <o.espitiamendoza@campus.unimib.it>
2022-04-11 22:16:45 +0200

    trying to update main files to avoid conficts

--- data_read.py
+++ data_read.py
@@ -1,64 +1,147 @@
+import os
+import os.path
 import re
-from os.path import join as joinpath
 import yaml
 import pandas as pd
+from os.path import join as joinpath
+from make_qrels_topics import get_qrels_topics,test_topic
 
 
+
+## clean
 def preprocessing(x):
-    # clean
     x = str(x).lower()
     x = re.sub(r'https?://\S+|www.\.\S+', '', x)
     x = re.sub(r'[^A-Za-zñáéíóúü0-9一-龠ぁ-ゔァ-ヴー々〆〤]+', ' ', x)
     return x
 
 
-def get_qrels_topics(df, lang):
-    # Qrels
-    topics = df[['query_id', 'query']]
-    topics = topics.drop_duplicates()
-    topics.columns = [['qid', 'query']]
+def index_file_for_each(cleaned_prod_cat, data_to_index):
+    '''
 
-    for ii, rows in topics.iterrows():
-        topics.at[ii, 'query'] = preprocessing(rows['query'])
-
-    qrels = df[['query_id', 'product_id', 'esci_label']]
-    qrels.columns = [['qid', 'docno', 'label']]
-
-    qrels = qrels.replace(to_replace={'exact': 3, 'substitute': 2, 'complement': 1, 'irrelevant': 0})
-
-    topics.to_csv('./subsets/%s_topics.csv' % lang, index=False, sep=':', header=False)
-    qrels.to_csv('./subsets/%s_qrels.csv' % lang, index=False, sep=',')
-    return qrels, topics
-
-
-def index_file(df_prod, lang='en'):
-    # get files to index in TREC format
-    df_prod.product_title = df_prod.product_title.apply(lambda x: preprocessing(x))
-    df_prod.product_bullet_point = df_prod.product_bullet_point.apply(lambda x: preprocessing(x))
-    df_prod.product_description = df_prod.product_description.apply(lambda x: preprocessing(x))
-    en_prod_sub = df_prod[
-        ['product_id', 'product_title', 'product_bullet_point', "product_brand", "product_description"]]
-    en_prod_sub.columns = ['docno', 'title', 'text', 'brand', 'desc']
-    en_prod_sub.brand = en_prod_sub.brand.str.lower()
-    en_prod_sub['title_text'] = en_prod_sub['title'] + en_prod_sub['text']
-    en_prod_sub['brand_text'] = en_prod_sub['brand'] + en_prod_sub['text']
-    en_prod_sub['title_brand_text'] = en_prod_sub['title'] + en_prod_sub['brand'] + en_prod_sub['text']
-    en_prod_sub['title_brand_des_text'] = en_prod_sub['title'] + en_prod_sub['brand'] + en_prod_sub['desc'] + \
-                                          en_prod_sub['text']
-    en_prod_sub['title_desc_text'] = en_prod_sub['title'] + en_prod_sub['desc'] + en_prod_sub['text']
-    en_prod_sub.to_csv('./subsets/%s_prod_sub.csv' % lang, index=False)
+    :param cleaned_prod_cat: Cleaned Product category Df
+    :param data_to_index: Which text to index
+    :return:
+    '''
+    for lang in ['us', 'jp', 'es']:
+        lang_prod = cleaned_prod_cat[cleaned_prod_cat['product_locale'] == lang]
+        dupli = lang_prod.duplicated(subset='product_id', keep=False)
+        lang_prod[dupli].sort_values('product_id')
+        lang_prod_sub = lang_prod[
+            ['product_id', data_to_index]]
+        lang_prod[data_to_index] = lang_prod[data_to_index].apply(lambda x: preprocessing(x))
+        lang_prod_sub.columns = ['docno', 'text']
+        lang_prod_sub.to_csv('./subsets/%s_prod_%s.csv' % (lang, data_to_index), index=False)
 
 
-def test_topic(df, lang='en'):
-    # Read test
-    topics = df[['query_id', 'query']]
-    topics = topics.drop_duplicates()
-    topics.columns = [['qid', 'query']]
+def clean_df_for_each(prod_cat):
+    subsets = ['product_bullet_point', 'product_title', 'product_description', 'product_brand']
+    for sub in subsets:
+        cleaned_prod_cat = prod_cat.dropna(subset=[sub])
+        ## duplicated and saving
+        index_file_for_each(cleaned_prod_cat, sub)
 
-    for ii, rows in topics.iterrows():
-        topics.at[ii, 'query'] = preprocessing(rows['query'])
-    topics.to_csv('./subsets/%s_test_topics.csv' % lang, index=False, sep=':', header=False)
-    return topics
+
+
+def for_2_comb(lang_prod, data_type, lang):
+    '''
+
+    For 2 combination of text
+    '''
+    lang_prod_s = lang_prod[
+        ['product_id', data_type[0], data_type[1]]]
+    lang_prod_s[data_type[0]] = lang_prod_s[data_type[0]].apply(lambda x: preprocessing(x))
+    lang_prod_s[data_type[1]] = lang_prod_s[data_type[1]].apply(lambda x: preprocessing(x))
+    lang_prod_s[f'''{data_type[0]}_{data_type[1]}'''] = lang_prod_s[data_type[0]] + lang_prod_s[data_type[1]]
+    lang_prod_sub = lang_prod_s[['product_id', f'''{data_type[0]}_{data_type[1]}''']]
+    lang_prod_sub.columns = ['docno', 'text']
+    lang_prod_sub.to_csv('./subsets/%s_prod_%s_%s.csv' % (lang, data_type[0], data_type[1]), index=False)
+
+
+def for_3_comb(lang_prod, data_type, lang):
+    '''
+
+    For 3 combination of text
+    '''
+    lang_prod_s = lang_prod[
+        ['product_id', data_type[0], data_type[1], data_type[2]]]
+    lang_prod_s[data_type[0]] = lang_prod_s[data_type[0]].apply(lambda x: preprocessing(x))
+    lang_prod_s[data_type[1]] = lang_prod_s[data_type[1]].apply(lambda x: preprocessing(x))
+    lang_prod_s[data_type[2]] = lang_prod_s[data_type[2]].apply(lambda x: preprocessing(x))
+    lang_prod_s[f'''{data_type[0]}_{data_type[1]}_{data_type[2]}'''] = lang_prod_s[data_type[0]] + lang_prod_s[
+        data_type[1]] + lang_prod_s[data_type[2]]
+    lang_prod_sub = lang_prod_s[['product_id', f'''{data_type[0]}_{data_type[1]}_{data_type[2]}''']]
+    lang_prod_sub.columns = ['docno', 'text']
+    lang_prod_sub.to_csv('./subsets/%s_prod_%s_%s_%s.csv' % (lang, data_type[0], data_type[1], data_type[2]),
+                         index=False)
+
+
+def for_4_comb(lang_prod, data_type, lang):
+    '''
+
+    For 4 combination of text
+    '''
+    lang_prod_s = lang_prod[
+        ['product_id', data_type[0], data_type[1], data_type[2], data_type[3]]]
+    lang_prod_s[data_type[0]] = lang_prod_s[data_type[0]].apply(lambda x: preprocessing(x))
+    lang_prod_s[data_type[1]] = lang_prod_s[data_type[1]].apply(lambda x: preprocessing(x))
+    lang_prod_s[data_type[2]] = lang_prod_s[data_type[2]].apply(lambda x: preprocessing(x))
+    lang_prod_s[data_type[3]] = lang_prod_s[data_type[3]].apply(lambda x: preprocessing(x))
+
+    lang_prod_s[f'''{data_type[0]}_{data_type[1]}_{data_type[2]}_{data_type[3]}'''] = \
+        lang_prod_s[data_type[0]] + lang_prod_s[data_type[1]] + lang_prod_s[data_type[2]] + lang_prod_s[data_type[3]]
+
+    lang_prod_sub = lang_prod_s[['product_id', f'''{data_type[0]}_{data_type[1]}_{data_type[2]}_{data_type[3]}''']]
+    lang_prod_sub.columns = ['docno', 'text']
+    lang_prod_sub.to_csv(
+        './subsets/%s_prod_%s_%s_%s_%s.csv' % (lang, data_type[0], data_type[1], data_type[2], data_type[3]),
+        index=False)
+
+
+### Different combinations
+def index_file_for_combine(cleaned_prod_cat, data_type):
+    '''
+    Create index file for combinations
+
+    '''
+    for lang in ['us', 'jp', 'es']:
+        print(lang, data_type)
+        lang_prod = cleaned_prod_cat[cleaned_prod_cat['product_locale'] == lang]
+        dupli = lang_prod.duplicated(subset='product_id', keep=False)
+        lang_prod[dupli].sort_values('product_id')
+
+        if len(data_type) == 2:
+            if not os.path.isfile('./subsets/%s_prod_%s_%s.csv' % (lang, data_type[0], data_type[1])):
+                for_2_comb(lang_prod, data_type, lang)
+
+        elif len(data_type) == 3:
+            if not os.path.isfile('./subsets/%s_prod_%s_%s_%s.csv' % (lang, data_type[0], data_type[1], data_type[2])):
+                for_3_comb(lang_prod, data_type, lang)
+
+
+        else:
+            if not os.path.isfile('./subsets/%s_prod_%s_%s_%s_%s.csv' % (
+                    lang, data_type[0], data_type[1], data_type[2], data_type[3])):
+                for_4_comb(lang_prod, data_type, lang)
+
+
+def clean_df_combine(prod_cat):
+    '''
+
+    Different combinations
+    '''
+    subsets = [['product_bullet_point', 'product_title'], ['product_bullet_point', 'product_description'],
+               ['product_bullet_point', 'product_brand'],
+               ['product_title', 'product_description'],
+               ['product_brand', 'product_description', 'product_bullet_point'],
+               ['product_title', 'product_description', 'product_bullet_point'],
+               ['product_title', 'product_brand', 'product_bullet_point'],
+               ['product_title', 'product_description', 'product_bullet_point', 'product_brand']]
+    for sub in subsets:
+        cleaned_prod_cat = prod_cat.dropna(subset=sub)
+        ## duplicated and saving
+        index_file_for_combine(cleaned_prod_cat, sub)
+
 
 
 if __name__ == '__main__':
@@ -70,40 +153,24 @@ if __name__ == '__main__':
     prod_cat = pd.read_csv(joinpath(data_path, config['collection']))
     train = pd.read_csv(joinpath(data_path, config['train']))
 
-    # Missing values
-    cleaned_prod_cat = prod_cat.dropna(
-        subset=['product_bullet_point', 'product_title', 'product_description']
-    )
-
-    # duplicated
-    dupli = cleaned_prod_cat.duplicated(subset='product_id', keep=False)
-    cleaned_prod_cat[dupli].sort_values('product_id')
-
-    # language split
-    en_prod = cleaned_prod_cat[cleaned_prod_cat['product_locale'] == 'us']
-    jp_prod = cleaned_prod_cat[cleaned_prod_cat['product_locale'] == 'jp']
-    es_prod = cleaned_prod_cat[cleaned_prod_cat['product_locale'] == 'es']
-
     # Get Topics and Qrels
     en_train = train[train['query_locale'] == 'us']
     jp_train = train[train['query_locale'] == 'jp']
     es_train = train[train['query_locale'] == 'es']
 
-    en_topics, en_qrels = get_qrels_topics(en_train, lang='en')
+    en_topics, en_qrels = get_qrels_topics(en_train, lang='us')
     jp_topics, jp_qrels = get_qrels_topics(jp_train, lang='jp')
     es_topics, es_qrels = get_qrels_topics(es_train, lang='es')
 
-    # get files for each languge to index ('en','es' and 'jp')
-    for i in ['en', 'es', 'jp']:
-        index_file(es_prod, lang=i)
+    # index for each text fields such as title, product brand etc.
+    if config['index_each_data']:
+        clean_df_for_each(prod_cat)
+
+    # index for combination of text fields.
+    clean_df_combine(prod_cat)
 
     test = pd.read_csv(joinpath(data_path, config['test']))
     # use for each language.
-    for lang in ['en', 'es', 'jp']:
-        if lang == 'en':
-            langs = 'us'
-            test_sub = test[test["query_locale"] == langs]
-            topics_test = test_topic(test_sub, lang=lang)
-        else:
-            test_sub = test[test["query_locale"] == lang]
-            topics_test = test_topic(test_sub, lang=lang)
+    for lang in ['us', 'es', 'jp']:
+        test_sub = test[test["query_locale"] == lang]
+        topics_test = test_topic(test_sub, lang=lang)
--- index_for_each.py
+++ index_for_each.py
@@ -1,36 +1,71 @@
 import os
-import yaml
+
 import pandas as pd
 import pyterrier as pt
-from src.data.utils.build_samples_4_reranking import build_samples_4_reranking
 
 if not pt.started():
-    pt.init()
+    pt.init(mem=8000)
 
+import yaml
+#version='snapshot', boot_packages=["com.github.terrierteam:terrier-prf:-SNAPSHOT"])
 
 def combine_result(data_path, model_name, data_to_index, dtype='train'):
+    '''
+
+    Combine results from different indexers.
+
+    :param data_path: Data path
+    :param model_name: BM25
+    :param data_to_index: Combination to index
+    :param dtype: train or test
+    :return:
+    '''
     final = []
-    for lang in ['en', 'es', 'jp']:
+    for lang in ['us', 'es', 'jp']:
         if os.path.isfile(f'''{data_path}{lang}_{data_to_index}_{model_name}_{dtype}.csv'''):
             sub_df = pd.read_csv(f'''{data_path}{lang}_{data_to_index}_{model_name}_{dtype}.csv''', index_col=0)
             if dtype == 'train':
-                sub_df = sub_df[['qid', 'docno', 'score']]
-                sub_df.qid = sub_df.qid.astype(str)
+                sub_df=sub_df[['qid','docno','score']]
+                sub_df.qid=sub_df.qid.astype(str)
                 sub_df.docno = sub_df.docno.astype(str)
-                # sub_df.columns=['query_id','product_id','score']
+                #sub_df.columns=['query_id','product_id','score']
             final.append(sub_df)
 
+
     df = pd.concat(final)
-    if dtype == 'test':
+    if dtype=='test':
         df.query_id = df.query_id.astype(str)
-        df.product_id = df.product_id.astype(str)
+        df.product_id=df.product_id.astype(str)
     df.to_csv(f'''{data_to_index}_{model_name}_{dtype}.csv''', index=None)
     return df
 
 
+def check_the_submission(df,data_path,model_name,data_to_index):
+    '''
+    Check the submission files
+
+    :param df: Combined Df
+    :param data_path: Data path
+    :param model_name: BM25
+    :param data_to_index: Combination
+    :return:
+    '''
+    cleaned_df=[]
+    for lang in ['us','es','jp']:
+        df_docs = pd.read_csv(f'''{data_path}{lang}_prod_sub.csv''')
+        product_id=df_docs.docno.values
+        product_id_combined = df[df['product_locale'] == lang].product_id.values
+        common_product_id = list(set(product_id).intersection(product_id_combined))
+        cleaned_df.append(df[(df['product_id'].isin(common_product_id)) & (df['product_locale'] == lang)])
+
+    final_df=pd.concat(cleaned_df)
+    final_df=final_df[['query_id','product_id']]
+    final_df.to_csv(f'''{data_to_index}_{model_name}_test.csv''', index=None)
+    return final_df
+
 def get_merged_qrels(data_path):
-    final = []
-    for lang in ['en', 'es', 'jp']:
+    final=[]
+    for lang in ['us','es','jp']:
         if os.path.isfile(f'''{data_path}{lang}_qrels.csv'''):
             qrels = pd.read_csv(f'''{data_path}{lang}_qrels.csv''', sep=',')
             qrels['label'] = qrels['label'].astype(int)
@@ -39,8 +74,13 @@ def get_merged_qrels(data_path):
     df = pd.concat(final)
     return df
 
-
 def get_qrels(data_path, lang):
+    '''
+
+    :param data_path: Data path for qrels specific to language (use make_qrels_topics.py before)
+    :param lang: 'us','es','jp'
+    :return:
+    '''
     qrels = pd.read_csv(f'''{data_path}{lang}_qrels.csv''', sep=',')
     qrels['label'] = qrels['label'].astype(int)
     qrels['qid'] = qrels['qid'].astype(str)
@@ -48,24 +88,42 @@ def get_qrels(data_path, lang):
 
 
 def index_each_lang(qe=False, eval=True, lang='en', index_path='', gs=False, data_to_index='title_text',
-                    change_settings=False):
+                    change_settings=False,num_results=100):
+
+    '''
+    Index each language
+
+    :param qe: Query Expansion
+    :param eval: Evaluatiton and scoring on train
+    :param lang: Specific Language ('us','es','jp')
+    :param index_path: Path to save index
+    :param gs: Grid Search
+    :param data_to_index: Combination of Data
+    :param change_settings: To use Grid Search settings
+    :param num_results: Total number of results.
+    :return:
+    '''
+
     data_type = f'''{lang}_{data_to_index}'''
-    df_docs = pd.read_csv(f'''{data_path}{lang}_prod_sub.csv''')
+    df_docs = pd.read_csv(f'''./subsets/{lang}_prod_{data_to_index}.csv''')
     if not os.path.exists(f'''{index_path}{data_type}/data.properties'''):
         indexer = pt.DFIndexer(f'''{index_path}{data_type}''', overwrite=True, verbose=True, Threads=8)
-        indexref3 = indexer.index(df_docs[data_to_index], df_docs[['docno', data_to_index]])
+        indexer.setProperty("tokeniser",
+                            "UTFTokeniser")  # Replaces the default EnglishTokeniser, which makes assumptions specific to English
+        indexer.setProperty("termpipelines", "Stopwords")
+        indexref = indexer.index(df_docs['text'], df_docs[['docno', 'text']])
     else:
-        indexref3 = pt.IndexRef.of(f'''{index_path}{data_type}/data.properties''')
+        indexref = pt.IndexRef.of(f'''{index_path}{data_type}/data.properties''')
 
     print('-----------------Indexed----------------\n')
 
-    if lang == 'en':
-        stemmer = 'Stopwords,PorterStemmer'
-    elif lang == 'es':
-        stemmer = 'Stopwords,SpanishSnowballStemmer'
+    if lang=='us':
+        stemmer='Stopwords,PorterStemmer'
+    elif lang=='es':
+        stemmer='Stopwords,SpanishSnowballStemmer'
     else:
-        stemmer = ''
-    BM25 = pt.BatchRetrieve(indexref3, num_results=30, wmodel="BM25",
+        stemmer=''
+    BM25 = pt.BatchRetrieve(indexref, num_results=num_results, wmodel="BM25",
                             controls={"c": 0.8, "bm25.k_1": 0.6, "bm25.k_3": 0.5}, properties={
             'tokeniser': 'UTFTokeniser',
             'termpipelines': stemmer, })
@@ -77,9 +135,9 @@ def index_each_lang(qe=False, eval=True, lang='en', index_path='', gs=False, dat
         topics = pt.io.read_topics(f'''{data_path}{lang}_topics.csv''', format='singleline', tokenise=True)
         f = pt.GridSearch(
             BM25,
-            {BM25: {"c": [0, 0.5, 0.8],
-                    "bm25.k_1": [0.4, 0.6, 0.9, 1.2],
-                    "bm25.k_3": [0.5, 2, 4, 6]
+            {BM25: {"c": [0.3, 0.5, 0.8, 0.9],
+                    "bm25.k_1": [0.3, 0.6, 0.9,1.2,1.4,1.6],
+                    "bm25.k_3": [0.5, 2, 4, 6,8]
                     }},
             topics,
             qrels,
@@ -90,7 +148,7 @@ def index_each_lang(qe=False, eval=True, lang='en', index_path='', gs=False, dat
 
     if qe:
         print('------------Query Expansion-------------')
-        RM3 = pt.rewrite.RM3(indexref3)
+        RM3 = pt.rewrite.RM3(indexref)
         BM25 = BM25 >> RM3 >> BM25
         model_name = 'BM25_RM3'
 
@@ -99,9 +157,6 @@ def index_each_lang(qe=False, eval=True, lang='en', index_path='', gs=False, dat
         qrels = get_qrels(data_path, lang)
         topics = pt.io.read_topics(f'''{data_path}{lang}_topics.csv''', format='singleline', tokenise=True)
         res = BM25.transform(topics)
-
-        build_samples_4_reranking(res, qrels)
-
         res.to_csv(f'''{data_path}{lang}_{data_to_index}_{model_name}_train.csv''')
         print(pt.Utils.evaluate(res, qrels, metrics=['ndcg_cut_10', 'ndcg_cut_20', 'ndcg']))
 
@@ -110,21 +165,38 @@ def index_each_lang(qe=False, eval=True, lang='en', index_path='', gs=False, dat
 
     submission_sample = res[['qid', 'docno']]
     submission_sample.columns = ['query_id', 'product_id']
-
+    submission_sample['product_locale'] = lang
     submission_sample.to_csv(f'''{data_path}{lang}_{data_to_index}_{model_name}_test.csv''')
+
 
 
 if __name__ == '__main__':
     with open('./config.yml') as f:
         config = yaml.load(f, Loader=yaml.FullLoader)['dataset']
-    data_path = 'data/processed/'
+    data_path = './subsets/'
     index_path = config["path"]
-    data_to_index = 'title_text'
-    for lang in ['es', 'jp']:
-        index_each_lang(qe=False, eval=True, lang=lang, index_path=index_path, gs=False)
-    combined_df = combine_result(data_path, 'BM25', data_to_index, dtype='test')
-    qrels = get_merged_qrels(data_path)
-    # print(pt.Utils.evaluate(combined_df, qrels, metrics=['ndcg_cut_10', 'ndcg_cut_20']))
-    # {'ndcg_cut_10': 0.23747557699680494, 'ndcg_cut_20': 0.22943625462979889}
-    # "{'ndcg_cut_10': 0.25315503702420733, 'ndcg_cut_20': 0.23043245567606602, 'ndcg': 0.21924521209667988}
+    data_to_indexs = ['product_bullet_point_product_title', 'product_bullet_point_product_description',
+                      'product_bullet_point_product_brand',
+                      'product_title_product_description',
+                      'product_brand_product_description_product_bullet_point',
+                      'product_title_product_description_product_bullet_point',
+                      'product_title_product_brand_product_bullet_point',
+                      'product_title_product_description_product_bullet_point_product_brand']
 
+    data_to_index = data_to_indexs[-2]
+    for lang in ['us','es', 'jp']:
+        index_each_lang(qe=False, eval=True, lang=lang, index_path=index_path, data_to_index=data_to_index, gs=False,num_results=200)
+
+
+    ## Combining different results for submission.
+    if config['create_submission_file']:
+        combined_df = combine_result(data_path, 'BM25', data_to_index, dtype='test')
+        cleaned_df = check_the_submission(combined_df, data_path, 'BM25', data_to_index)
+
+    if config['evaluate_train']:
+        combined_df = combine_result(data_path, 'BM25', data_to_index, dtype='train')
+        qrels = get_merged_qrels(data_path)
+        pt.Utils.evaluate(combined_df, qrels, metrics=['ndcg_cut_10', 'ndcg_cut_20'])
+
+    # {'ndcg_cut_10': 0.23747557699680494, 'ndcg_cut_20': 0.22943625462979889} num_results=20
+    # {'ndcg_cut_10': 0.27361675817105907, 'ndcg_cut_20': 0.26223407618626343} num_results=100
